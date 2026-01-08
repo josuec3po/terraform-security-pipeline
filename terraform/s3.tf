082173d0de96a4548bf745487eddcee72a085f47
Josué Franco Braga <josuefbraga@outlook.com>
2026-01-08 20:11:10 -0300

    -fix bucket tfsec

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -65,3 +65,37 @@ resource "aws_s3_bucket_logging" "secure_bucket_logging" {
   target_bucket = aws_s3_bucket.log_bucket.id
   target_prefix = "access-logs/"
 }
+
+resource "aws_s3_bucket_public_access_block" "log_bucket_block" {
+  bucket = aws_s3_bucket.log_bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+resource "aws_s3_bucket_versioning" "log_bucket_versioning" {
+  bucket = aws_s3_bucket.log_bucket.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+resource "aws_s3_bucket_server_side_encryption_configuration" "log_bucket_encryption" {
+  bucket = aws_s3_bucket.log_bucket.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm     = "aws:kms"
+      kms_master_key_id = aws_kms_key.s3_key.arn
+    }
+  }
+}
+
+resource "aws_s3_bucket_logging" "log_bucket_logging" {
+  bucket        = aws_s3_bucket.log_bucket.id
+  target_bucket = aws_s3_bucket.log_bucket.id
+  target_prefix = "self-logs/"
+}
+
+
