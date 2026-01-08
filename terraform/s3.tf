9ea66d3536b5b59d7d71b354f27724245ba4614f
Josué Franco Braga <josuefbraga@outlook.com>
2026-01-08 17:54:43 -0300

    -test tfsec crash

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,6 +1,10 @@
 //Codigo de crash intencional para teste da ferramenta tfsec
 //Cria um bucket S3 com ACL pública, o que é uma má prática de segurança
-resource "aws_s3_bucket_acl" "insecure_bucket_acl" {
+resource "aws_s3_bucket" "insecure_bucket" {
   bucket = "tfsec-insecure-bucket-example-123456"
+}
+
+resource "aws_s3_bucket_acl" "insecure_bucket_acl" {
+  bucket = aws_s3_bucket.insecure_bucket.id
   acl    = "public-read"
 }
